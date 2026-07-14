70d14c6b9fd17dde23673225ec9599509574736b
selezenart <artvs14@gmail.com>
2026-07-14 15:49:40 +0200

    done alembic part

---
+++ ft_alembic_2.py
@@ -0,0 +1,6 @@
+import alchemy.elements as e
+
+if __name__ == "__main__":
+    print("=== Alembic 2 ===")
+    print("Accessing alchemy/elements.py using 'import ...' structure")
+    print(f"Testing create_earth: {e.create_earth()}")
---
+++ ft_alembic_3.py
@@ -0,0 +1,7 @@
+from alchemy import elements
+
+if __name__ == "__main__":
+    print("=== Alembic 3 ===")
+    print("Accessing alchemy/elements.py using "
+          "'from ... import ...' structure")
+    print("Testing create_air:", elements.create_air())
