322ff08e260bd8e54979b0275a28f54c93f59112
selezenart <artvs14@gmail.com>
2026-07-21 10:53:48 +0200

    fized flake8

--- ft_kaboom_1.py
+++ ft_kaboom_1.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     print("Access to alchemy/grimoire/dark_spellbook.py directly")
     print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
     from alchemy.grimoire.dark_spellbook import dark_spell_record
+    print(dark_spell_record("Fantasy", "Earth, wind and fire"))
